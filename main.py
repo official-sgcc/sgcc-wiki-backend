2b9ce711c6f36838d00b27980ccd5a893ccbcb2c
jiy2745 <jiy2745@gmail.com>
2026-04-06 21:57:39 +0900

    feat: add CRUD APIs for documents

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import create_engine, Session, select, SQLModel
-from schemas.wiki_doc import WikiDoc
+from schemas.wiki_doc import WikiDoc, WikiDocCreate, WikiDocUpdate
 from schemas.permissions import Permissions
+from datetime import datetime
 
 app = FastAPI()
 
 engine = create_engine("sqlite:///wiki.db")
 SQLModel.metadata.create_all(engine)
 
-# Hello World GET
-@app.get("/")
-async def root():
-    return {'message': 'Hello World'}
-
-@app.get('/document/{title}')
-async def get_document(title: str):
-    pass
-
-@app.get("/documents")
-async def get_all_documents():
+# Get documents
+@app.get('/documents')
+async def get_documents(keyword: str | None = None):
     with Session(engine) as session:
-        documents = session.exec(select(WikiDoc)).all()
-        return documents
+        if keyword:
+            statement = select(WikiDoc).where(
+                WikiDoc.title.contains(keyword) | WikiDoc.content.contains(keyword)
+            )
+            return session.exec(statement).all()
+        else:
+            return session.exec(select(WikiDoc)).all()
 
+# Create document
+@app.post('/documents')
+async def create_document(doc_in: WikiDocCreate):
+    with Session(engine) as session:
+        if session.get(WikiDoc, doc_in.title):
+            raise HTTPException(status_code=400, detail='There is already a document with the same name.')
+        
+        doc = WikiDoc(**doc_in.model_dump())
+        doc.updated_at = datetime.now()
+        
+        session.add(doc)
+        session.commit()
+        session.refresh(doc)
+        return doc
+
+# Read document
+@app.get('/documents/{title}')
+async def get_document(title: str):
+    with Session(engine) as session:
+        doc = session.get(WikiDoc, title)
+        if not doc:
+            raise HTTPException(status_code=404, detail='Cannot find a document with the corresponding name.')
+        return doc
+
+# Update document
+@app.put('/documents/{title}')
+async def update_document(title: str, update_data: WikiDocUpdate):
+    with Session(engine) as session:
+        if not (doc := session.get(WikiDoc, title)):
+            raise HTTPException(status_code=404, detail='Cannot find document to update')
+
+        if update_data.content is not None:
+            doc.content = update_data.content
+            
+        if update_data.tags is not None:
+            doc.tags = update_data.tags
+        
+        doc.updated_at = datetime.now()
+        
+        session.add(doc)
+        session.commit()
+        session.refresh(doc)
+        return doc
+
+# Delete document
+@app.delete('/documents/{title}')
+async def delete_document(title: str):
+    with Session(engine) as session:
+        if not (doc := session.get(WikiDoc, title)):
+            raise HTTPException(status_code=404, detail='Cannot find document to delete')
+
+        session.delete(doc)
+        session.commit()
+        return {'message': f'The document named {title} has been deleted.'}
--- schemas/wiki_doc.py
+++ schemas/wiki_doc.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, Field, Relationship
+from pydantic import BaseModel
 from datetime import datetime
 from sqlalchemy.types import JSON
 from .permissions import Permissions
@@ -6,6 +7,15 @@ from .permissions import Permissions
 class WikiDoc(SQLModel, table=True):
     title: str = Field(primary_key=True)
     content: str
-    tags: list[str] = Field(sa_type=JSON)
+    tags: list[str] = Field(default_factory=list, sa_type=JSON)
     updated_at: datetime
     permissions: Permissions | None = Relationship()
+
+class WikiDocCreate(BaseModel):
+    title: str
+    content: str
+    tags: list[str] = Field(default_factory=list)
+
+class WikiDocUpdate(BaseModel):
+    content: str | None = None
+    tags: list[str] | None = None
