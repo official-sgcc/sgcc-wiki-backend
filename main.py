9772dfbf89e71da05b7ada42351b23422134035f
jiy2745 <jiy2745@gmail.com>
2026-05-12 17:34:43 +0900

    fix: correct typo

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ app = FastAPI()
 
 load_dotenv()
 
-FRONTEND_URL =  os.getenv('FRONTEND_URL', 'http://localhost:5173')
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[FRONTEND_URL, 'http://localhost:5173', 'http://127.0.0.1:5173'],
     allow_credentials=True,
-    allow_method=['*'],
+    allow_methods=['*'],
     allow_headers=['*']
 )
 
