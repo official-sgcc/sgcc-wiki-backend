168cf70de21481fd2b6774bbe683a19f4b517ad5
jiy2745 <jiy2745@gmail.com>
2026-05-12 17:25:23 +0900

    chore: add some CORS URL

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ app = FastAPI()
 
 load_dotenv()
 
-FRONTEND_URL =  os.getenv('FRONTEND_URL') or 'http://localhost:5173'
+FRONTEND_URL =  os.getenv('FRONTEND_URL', 'http://localhost:5173')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=[FRONTEND_URL, 'http://localhost:5173', 'http://127.0.0.1:5173'],
     allow_credentials=True,
     allow_method=['*'],
     allow_headers=['*']
